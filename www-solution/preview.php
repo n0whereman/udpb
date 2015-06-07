e6c8506b453268b0f9bde037e293418d006a8f05
root <root@debian>
2015-06-07 10:20:34 -0400

    solution

---
+++ www-solution/preview.php
@@ -0,0 +1,12 @@
+<?php
+   //A4 - trosku blba funkcia, huh?
+   $file = $_GET['file'];
+   if(isset($file))
+   {
+       include("$file");
+   }
+   else
+   {
+       include("index.php");
+   }
+   ?>
